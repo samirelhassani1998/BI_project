caa8c583197b226d862e3b2089a27c21ccc269a9
EL HASSANI Samir <115094184+samirelhassani1998@users.noreply.github.com>
2023-03-20 00:40:45 +0100

    Update parcoursup.py

--- parcoursup.py
+++ parcoursup.py
@@ -17,18 +17,18 @@ st.plotly_chart(fig)
 
 # Répartition des vœux et des propositions d'admission par formation, type de formation (sélective ou non) et établissement
 st.header("Répartition des vœux et des propositions d'admission")
-fig2 = px.scatter(data, x="type_formation", y="nb_voeux", color="etablissement")
+fig2 = px.scatter(data, x="select_form", y="voe_tot", color="g_ea_lib_vx")
 st.plotly_chart(fig2)
 
 # Identifier les formations et les établissements les plus demandés et les plus sélectifs
 st.header("Formations et établissements les plus demandés et les plus sélectifs")
 # Utilisez les colonnes appropriées de vos données pour l'analyse
-top_demandes = data.nlargest(10, "nb_voeux")
-top_selectifs = data.nlargest(10, "taux_selectivite")
+top_demandes = data.nlargest(10, "voe_tot")
+top_selectifs = data.nlargest(10, "select_form")
 st.write("Formations les plus demandées:")
-st.write(top_demandes[["formation", "etablissement", "nb_voeux"]])
+st.write(top_demandes[["lib_for_voe_ins", "g_ea_lib_vx", "voe_tot"]])
 st.write("Formations les plus sélectives:")
-st.write(top_selectifs[["formation", "etablissement", "taux_selectivite"]])
+st.write(top_selectifs[["lib_for_voe_ins", "g_ea_lib_vx", "select_form"]])
 
 # Autres analyses (tendances, corrélations, géographiques, comparatives)
 # Vous pouvez continuer à ajouter des sections pour chaque type d'analyse que vous souhaitez réaliser,
