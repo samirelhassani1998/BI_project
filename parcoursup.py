21b71db65fa8ae3644c4a549d4c8a41cd6667118
EL HASSANI Samir <115094184+samirelhassani1998@users.noreply.github.com>
2023-03-23 15:33:53 +0100

    Update parcoursup.py

--- parcoursup.py
+++ parcoursup.py
@@ -1,15 +1,1 @@
-import pandas as pd
-import streamlit as st
 
-# Charger les données
-@st.cache
-def load_data():
-    data_url = 'https://www.data.gouv.fr/fr/datasets/r/1d916b7c-bd4c-4951-845a-70f7ad7c17db'
-    df = pd.read_csv(data_url, delimiter=';', encoding='utf-8', low_memory=False)
-    return df
-
-df = load_data()
-
-# Afficher les données
-st.title("Données sur les admissions Parcoursup")
-st.write(df)
